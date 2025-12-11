d4f9d782bce4026c3760a34b1bf564ea0c7c2dfe
Ben Goddard <goddard.ben04@gmail.com>
2025-12-11 15:29:48 -0700

    Finished CLI setup and organized files and functions

--- lib/main.py
+++
@@ -1,5 +0,0 @@
-import argparse
-from lib.utils import save_data, fetch_inventory
-
-def main():
-    parser = argparse.ArgumentParser(description="Inventory Manager CLI")
--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,5 @@
 import json
 import os
-import requests
 
 DATA_FILE = "data.json"
 
@@ -20,16 +19,3 @@ def save_data(data):
             json.dump(data, f, indent=4)
     except IOError as e:
         print(f"Error saving data to {DATA_FILE}: {e}")
-
-def fetch_inventory(barcode, inv):
-    URL = f"https://world.openfoodfacts.net/api/v2/product/{barcode}?fields=product_name,nutriscore_data"
-    try:
-        response = requests.get(URL)
-        data = response.json()
-    except requests.exceptions.RequestException as e:
-        return (f"Error: Failed to fetch data from API: {e}")
-
-    new_id = max((i["id"] for i in inv), default=0) + 1
-    new_item = {"id": new_id, "title": data["product"]["product_name"]}
-    inv.append(new_item)
-    print(new_item)
